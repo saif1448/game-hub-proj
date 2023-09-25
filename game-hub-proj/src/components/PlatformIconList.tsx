f07aec02f3a6556da316843b103c3d20ddaab848
Saif Mahmud Parvez <saif.mahmud.parvez@gmail.com>
2023-09-25 16:38:38 +0600

    Displayed Icons

---
+++ game-hub-proj/src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Platform } from '../hooks/useGames'
+import { HStack, Icon, Text } from '@chakra-ui/react'
+
+import {
+    FaWindows,
+    FaPlaystation,
+    FaXbox,
+    FaApple,
+    FaLinux,
+    FaAndroid
+} from 'react-icons/fa'
+
+import { MdPhoneIphone } from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import { BsGlobe } from 'react-icons/bs'
+import { IconType } from 'react-icons'
+
+interface Props {
+    platforms: Platform[]
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: {[key: string]: IconType} = {
+        pc: FaWindows,
+        plastation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+        android: FaAndroid
+    }
+    return (
+        <HStack marginY={3}>
+            {platforms.map((platform) => (
+                <Icon as={iconMap[platform.slug]} color='gray.500'/>
+            ))}
+        </HStack>
+    )
+}
+
+export default PlatformIconList
