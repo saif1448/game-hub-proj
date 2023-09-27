051d00d0f7b266402e6ceba07a2d2337fb8c753e
Saif Mahmud Parvez <saif.mahmud.parvez@gmail.com>
2023-09-27 11:27:18 +0600

    Show a spinner while fetching the genres

--- game-hub-proj/src/components/GenreList.tsx
+++ game-hub-proj/src/components/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGeneres from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
 
-    const { data } = useGeneres();
+    const { data, isLoading, error } = useGeneres();
+
+    if (isLoading) return <Spinner/>
+
+    if (error) return null
 
     return (
         <List>
