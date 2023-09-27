399c24f8c4f1d245f99b3f4981ab96b17ac605b1
Saif Mahmud Parvez <saif.mahmud.parvez@gmail.com>
2023-09-27 11:23:05 +0600

    Display genres

--- game-hub-proj/src/components/GameCardContainer.tsx
+++ game-hub-proj/src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props{
 
 const GameCardContainer = ({children}: Props) => {
     return (
-        <Box width='200px'
+        <Box 
             borderRadius={10}
             overflow={'hidden'}>
             {children}
--- game-hub-proj/src/components/GenreList.tsx
+++ game-hub-proj/src/components/GenreList.tsx
@@ -1,13 +1,21 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import useGeneres from '../hooks/useGenres'
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
 
     const { data } = useGeneres();
 
     return (
-        <ul>
-            {data.map((genre) => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map((genre) =>
+                <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                        <Text fontSize='lg '>{genre.name}</Text>
+                    </HStack>
+                </ListItem>)}
+        </List>
     )
 }
 
